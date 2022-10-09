b564c1daf880e7bdbedef2f29dd7000e415215b0
YoussefOsama200 <youssef.ali001@eng-st.cu.edu.eg>
2022-10-09 20:00:33 +0200

    adding screen shot

---
+++ eclipse workspace/automatic_fan_control/Debug/main.d
@@ -0,0 +1,16 @@
+main.o main.o: ../main.c ../lm35_sensor.h ../std_types.h ../lcd.h \
+  ../adc.h ../dc_motor.h ../common_macros.h ../pwm.h
+
+../lm35_sensor.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../adc.h:
+
+../dc_motor.h:
+
+../common_macros.h:
+
+../pwm.h:
---
+++ eclipse workspace/automatic_fan_control/Debug/pwm.d
@@ -0,0 +1,10 @@
+pwm.o pwm.o: ../pwm.c ../pwm.h ../common_macros.h ../std_types.h \
+  ../gpio.h
+
+../pwm.h:
+
+../common_macros.h:
+
+../std_types.h:
+
+../gpio.h:
